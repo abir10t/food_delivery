8845a132bb99cb53a38f4e55db67fdb0b81092ad
abir10t <aabir.10t@gmail.com>
2022-02-24 11:17:55 +0600

    trying to add api

---
+++ lib/data/repository/popular_product_repo.dart
@@ -0,0 +1,15 @@
+import 'package:food_delivery/data/api/api_client.dart';
+import 'package:get/get.dart';
+
+class PopularProductRepo extends GetxService
+{
+
+  PopularProductRepo({required this.apiClient});
+  final  ApiClient apiClient;
+
+  Future<Response> getPopularProductList()async {
+   return await apiClient.getData("https://www.dbestech.com/api/product");
+  }
+
+}
+
---
+++ lib/helper/dependencies.dart
@@ -0,0 +1,22 @@
+import 'package:food_delivery/controllers/popular_product_controller.dart';
+import 'package:food_delivery/data/api/api_client.dart';
+import 'package:food_delivery/data/repository/popular_product_repo.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/src/extension_instance.dart';
+
+Future<void> init()async
+{
+/// api client
+Get.lazyPut(()=>ApiClient(appBaseUrl: "https://www.dbestech.com"));
+
+/// controllers
+Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+
+
+Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
+
+
+
+}
+
+/// 5 min 50s
